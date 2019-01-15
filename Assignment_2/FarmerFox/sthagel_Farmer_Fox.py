8ae01525ff6c075b8e6f2d063a57dc349fe58bd7
Stephan Hagel <sthagel@uw.edu>
2019-01-15 15:22:08 -0800

    Initial commit for Assignment 2.

---
+++ Assignment_2/FarmerFox/sthagel_Farmer_Fox.py
@@ -0,0 +1,9 @@
+'''sthagel_Farmer_Fox.py
+by Stephan Hagel
+
+Assignment 2, in CSE 415, Winter 2019.
+
+This file contains my problem formulation for the problem of
+the Farmer, Fox, Chicken, and Grain.
+'''
+
